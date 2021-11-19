8a2ab8502b7626434a2a2ac8b2a419b868e029f8
Vu Thanh Long <sepdra143@gmail.com>
2021-11-19 07:34:43 +0000

    fix music_control

--- cogs/music_control.py
+++ cogs/music_control.py
@@ -1,16 +1,12 @@
 import discord
 from discord.ext import commands, tasks
-# import DiscordUtils
 from utils.Music import Music
 import asyncio
 import json
 
-# music = None
-
 class MusicControl(commands.Cog):
   def __init__(self,client):
     self.client = client
-    # self.music = DiscordUtils.Music()
     self.music = Music()
     self.config = None
 
@@ -28,11 +24,10 @@ class MusicControl(commands.Cog):
   @commands.Cog.listener()
   async def on_voice_state_update(self,user, before, after):
     if after.channel is None and user.id == self.client.user.id:
-      # self.music = DiscordUtils.Music()
       self.music = Music()
       self.checkPlaying.cancel()
 
-  @commands.command(brief = "Make the bot join your current voice channel.", help = "Make the bot join your current voice channel. You need to join a voice channel first!")
+  @commands.command()
   @commands.guild_only()
   async def join(self,ctx):
     authorvoice = ctx.author.voice
@@ -43,7 +38,7 @@ class MusicControl(commands.Cog):
     await asyncio.sleep(120)
     self.checkPlaying.start(ctx)
 
-  @commands.command(brief = "Make the bot leave the current voice channel.", help = "Make the bot leave the current voice channel.")
+  @commands.command()
   @commands.guild_only()
   async def leave(self,ctx):
     authorvoice = ctx.author.voice
@@ -55,7 +50,7 @@ class MusicControl(commands.Cog):
     await ctx.voice_client.disconnect()
     await ctx.send(embed=discord.Embed(title="",description=self.config["leave"]))
 
-  @commands.command(aliases=["p"],brief = "Play music from the given URL or search for a track and play", help = "play / p [link or search query]\nLoads your input and adds it to the queue; If there is no playing track, then it will start playing")
+  @commands.command(aliases=["p"])
   @commands.guild_only()
   async def play(self,ctx,*,url):
     authorvoice = ctx.author.voice
@@ -78,40 +73,40 @@ class MusicControl(commands.Cog):
         await ctx.send(embed=discord.Embed(title="",description=eval("f'{}'".format(self.config["queue_song"]))))
     self.checkPlaying.start(ctx)
 
-  @commands.command(brief = "Pause the player.", help = "Pause the player.")
+  @commands.command()
   @commands.guild_only()
   async def pause(self,ctx):
     player = self.music.get_player(guild_id=ctx.guild.id)
     song = await player.pause()
     await ctx.send(embed=discord.Embed(title="",description=eval("f'{}'".format(self.config["pause"]))))
     
-  @commands.command(brief = "Resume the player.", help = "Resume the player.")
+  @commands.command()
   @commands.guild_only()
   async def resume(self,ctx):
     player = self.music.get_player(guild_id=ctx.guild.id)
     song = await player.resume()
     await ctx.send(embed=discord.Embed(title="",description=eval("f'{}'".format(self.config["resume"]))))
 
-  @commands.command(aliases=["q"],brief = "Display the current song queue.", help = "Display the current song queue.")
+  @commands.command(aliases=["q"])
   @commands.guild_only()
   async def queue(self,ctx):
     player = self.music.get_player(guild_id=ctx.guild.id)
-    embed = discord.Embed(title="Song queue:",description="")
+    embed = discord.Embed(title=self.config["queue"],description="")
     order=1
     for song in player.current_queue():
       embed.description += f"{order}, [{song.name}]({song.url})\n"
       order+=1
-    embed.add_field(name='\u200b', value="This is the end of the queue!\nUse -play to add more")
+    embed.add_field(name='\u200b', value=eval("f'{}'".format(self.config["queue_end"])))
     await ctx.send(embed = embed)
 
-  @commands.command(brief = "Stop the player and clear the queue.", help = "Stop the player and clear the queue.")
+  @commands.command()
   @commands.guild_only()
   async def stop(self,ctx):
     player = self.music.get_player(guild_id=ctx.guild.id)
     await player.stop()
     await ctx.send(embed=discord.Embed(title="",description=self.config["stop"]))
 
-  @commands.command(aliases=["np"],brief = "Display the currently playing song.", help = "Display the currently playing song.")
+  @commands.command(aliases=["np"])
   @commands.guild_only()
   async def nowplaying(self,ctx):
     player = self.music.get_player(guild_id=ctx.guild.id)
@@ -123,21 +118,21 @@ class MusicControl(commands.Cog):
     embed.add_field(name=self.config["np_duration"], value=f'{int(int(song.duration)/60)}:{int(song.duration)%60}',inline = True)
     await ctx.send(embed=embed)
     
-  @commands.command(brief = "Skip to the next song.", help = "Skip to the next song.")
+  @commands.command()
   @commands.guild_only()
   async def skip(self,ctx):
     player = self.music.get_player(guild_id=ctx.guild.id)
     data = await player.skip(force=True)
     await ctx.send(embed=discord.Embed(title="",description=eval("f'{}'".format(self.config["skip"]))))
 
-  @commands.command(brief = "Change the volume.", help = "volume [0-100]\nChange the volume. Values are 0-100")
+  @commands.command()
   @commands.guild_only()
   async def volume(self,ctx, vol):
     player = self.music.get_player(guild_id=ctx.guild.id)
     song, volume = await player.change_volume(float(vol) / 100) # volume should be a float between 0 to 1
     await ctx.send(embed=discord.Embed(title="",description=eval("f'{}'".format(self.config["volume"]))))
     
-  @commands.command(brief = "Remove the specified track from the queue.", help = "remove[index]\nRemoves the specified track from the queue.")
+  @commands.command()
   @commands.guild_only()
   async def remove(self,ctx, index:int):
     player = self.music.get_player(guild_id=ctx.guild.id)
