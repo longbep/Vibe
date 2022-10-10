aa971c90740db529aa75e3de7d8970fe77dbf5e6
Vu Thanh Long <sepdra143@gmail.com>
2022-10-10 10:25:10 +0000

    [UPDATE] change status command

--- cogs/cog_control.py
+++ cogs/cog_control.py
@@ -1,32 +1,44 @@
+import discord
 from discord.ext import commands
 
+
 class CogControl(commands.Cog):
+    def __init__(self, client):
+        self.client = client
 
-  def __init__(self,client):
-    self.client = client
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def load(self, ctx, extension):
+        self.client.load_extension(f'cogs.{extension}')
+        await ctx.send("Load successful")
 
-  @commands.command(hidden=True)
-  @commands.is_owner()
-  async def load(self,ctx,extension):
-    self.client.load_extension(f'cogs.{extension}')
-    await ctx.send("Load successful")
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def unload(self, ctx, extension):
+        self.client.unload_extension(f'cogs.{extension}')
+        await ctx.send("Unload successful")
 
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def reload(self, ctx, extension):
+        self.client.reload_extension(f'cogs.{extension}')
+        if ctx.guild.me.voice is not None:
+            await ctx.voice_client.disconnect()
+        await ctx.send("Reload successful")
 
-  @commands.command(hidden=True)
-  @commands.is_owner()
-  async def unload(self,ctx,extension):
-    self.client.unload_extension(f'cogs.{extension}')
-    await ctx.send("Unload successful")
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def status(self, ctx):
+        totalMember = 0
+        for guild in self.client.guilds:
+            totalMember += guild.member_count
 
-
-  @commands.command(hidden=True)
-  @commands.is_owner()
-  async def reload(self,ctx,extension):
-    self.client.reload_extension(f'cogs.{extension}')
-    if ctx.guild.me.voice is not None:
-      await ctx.voice_client.disconnect()
-    await ctx.send("Reload successful")
+        await self.client.change_presence(activity=discord.Activity(
+            name=
+            f"-help | {len(self.client.guilds)} servers | {totalMember} members",
+            type=discord.ActivityType.listening))
+        await ctx.send("Reload status successful")
 
 
 def setup(client):
-  client.add_cog(CogControl(client))
+    client.add_cog(CogControl(client))
